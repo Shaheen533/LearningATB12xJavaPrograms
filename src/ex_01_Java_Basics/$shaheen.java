93500c0b2e13a47487d85c786b9baf4c42ba9c83
shaheen533 <shaheensoudagar1@gmail.com>
2025-06-22 23:52:13 +0530

    commit

---
+++ src/ex_01_Java_Basics/$shaheen.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class $shaheen {
+    public static void main(String[] args) {
+        System.out.println();
+        /* $shaheen,ABC123,_SHAHEEN class names are allowed
+        * _ is allowed but sometimes it throws error
+         */
+    }
+}
