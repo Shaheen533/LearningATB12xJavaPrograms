7ea9a62429a0e1ea9e42853d48c8573f96a2995a
shaheen533 <shaheensoudagar1@gmail.com>
2025-06-29 00:40:58 +0530

    Literals

---
+++ src/ex_03_Literals/Lab031_Non_Primitive_Literals.java
@@ -0,0 +1,19 @@
+package ex_03_Literals;
+
+public class Lab031_Non_Primitive_Literals {
+    public static void main(String[] args) {
+        //Primitive Data type
+        //Max, Min Size defined
+
+        byte b = 10;//1 Byte -> 8 Bits
+        int age = 65;//4 Byte -> 32 Bits
+
+        //Non primitive, Reference data types
+        //No size,max,min
+        //Byte = 8, Bits =64 Eg :Strings and Arrays(Mutliple integers)
+        String name = "Shaheen";//String is bunch of multiple characters
+        int[] arrays_of_items = new int[10];
+       
+
+    }
+}
